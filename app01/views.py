ddc6e1eaf87ee557f6b32902da3a01949ef4d1e3
lixun <lixun@yy.com>
2022-09-09 10:51:21 +0800

    css 样式需要从css 官网拷贝

--- app01/views.py
+++ app01/views.py
@@ -1,3 +1,14 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def depart_list(request):
+    """部门列表
+
+    Args:
+        request (_type_): _description_
+    """
+
+    return render(request,'depart_list.html')
+
--- staff_web/urls.py
+++ staff_web/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import imp
 from django.contrib import admin
 from django.urls import path
+from app01 import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('depart/list/', views.depart_list),
 ]
